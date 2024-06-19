09ade13c4ffcacfe0098b81f09ad6923d1c15c6e
celsobritodev <celsinhobrito@gmail.com>
2024-06-19 09:21:55 -0300

    manipulando cookies

--- src/main/java/geral/CarrinhoServlet.java
+++ src/main/java/geral/CarrinhoServlet.java
@@ -13,6 +13,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class CarrinhoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private static final String COOKIE_NAME = "produtos";
+	//private static final String COOKIE_NAME = "produtos1";
+
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -23,13 +27,19 @@ public class CarrinhoServlet extends HttpServlet {
 		PrintWriter pw = response.getWriter();
 
 		if (cookies != null)
-			for (Cookie ci : cookies)
-				if (ci.getName().equals("produtos"))
+			for (Cookie ci : cookies) {
+				String sCookieName=ci.getName(); 
+				if (sCookieName.equals(COOKIE_NAME))
 					cookieProdutos = ci;
+			}
 		if (cookieProdutos != null)
-			pw.append("Produtos do carrinho: " + cookieProdutos.getValue());
+			if(!cookieProdutos.getValue().isEmpty()) 
+  		      pw.append("PRODUTOS: " + cookieProdutos.getValue());
+			else
+			  pw.append("PRODUTOS: NÃO HÁ NENHUM PRODUTO!");
 
 	}
+	
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -41,16 +51,19 @@ public class CarrinhoServlet extends HttpServlet {
 		PrintWriter pw = response.getWriter();
 
 		if (cookies != null)
-			for (Cookie ci : cookies)
-				if (ci.getName().equals("produtos"))
+			for (Cookie ci : cookies) {
+				String sCookieName=ci.getName(); 
+				if (sCookieName.equals(COOKIE_NAME))
 					cookieProdutos = ci;
+			}
 		Cookie cp = null;
 		if (produto != null) {
 			if (cookieProdutos != null) {
-				cp = new Cookie("produtos", cookieProdutos.getValue() + "/" + produto);
+				cp = new Cookie(COOKIE_NAME, cookieProdutos.getValue() + "/" + produto);
 			} else {
-				cp = new Cookie("produtos", produto);
+				cp = new Cookie(COOKIE_NAME, produto);
 			}
+			response.addCookie(cp);
 			response.setStatus(201);
 		} else {
 			response.setStatus(400);
@@ -68,10 +81,11 @@ public class CarrinhoServlet extends HttpServlet {
 		PrintWriter pw = response.getWriter();
 
 		if (cookies != null)
-			for (Cookie ci : cookies)
-				if (ci.getName().equals("produtos"))
+			for (Cookie ci : cookies) {
+				String sCookieName=ci.getName(); 
+				if (sCookieName.equals(COOKIE_NAME))
 					cookieProdutos = ci;
-
+			}  
 		if (produto != null && cookieProdutos != null) {
 			String strProds = cookieProdutos.getValue();
 			String prods[] = strProds.split("/");
@@ -80,8 +94,9 @@ public class CarrinhoServlet extends HttpServlet {
 			for (String p : prods)
 				if (!p.toUpperCase().equals(produto.toUpperCase()))
 					lp.add(p);
-			strProds = String.join("produtos", strProds);
-			Cookie cp = new Cookie("produtos", strProds);
+
+			strProds = String.join("/", lp);
+			Cookie cp = new Cookie(COOKIE_NAME, strProds);
 			response.addCookie(cp);
 			response.setStatus(200);
 		} else {
